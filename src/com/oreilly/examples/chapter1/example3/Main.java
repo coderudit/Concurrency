920cccfd5a3f429bf4836dc7ef231a7d982f1eef
Udit Gandhi <coder.udit@gmail.com>
2022-01-13 13:48:52 -0400

    Thread examples.

---
+++ src/com/oreilly/examples/chapter1/example3/Main.java
@@ -0,0 +1,20 @@
+package com.oreilly.examples.chapter1.example3;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        FileSearch searcher = new FileSearch("C:\\Windows",
+                "explorer.exe");
+        Thread thread = new Thread(searcher);
+        thread.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
+    }
+}
+
